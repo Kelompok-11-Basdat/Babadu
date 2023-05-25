9f1da06ee52b5423859bcf875090c8d7ec4ff161
andreshapradana <andresha.pradana@gmail.com>
2023-05-25 14:58:25 +0700

    dashboard atlet

--- dashboard_atlet/views.py
+++ dashboard_atlet/views.py
@@ -1,51 +1,71 @@
 from django.shortcuts import render
 from django.db import connection
+from django.http import HttpResponse
+from django.shortcuts import render
 
+from django.shortcuts import render, redirect
+from django.db import connection, InternalError
+
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
+
+from util.query import *
+from atlet.query import *
+from Babadu.helper.function import *
 def show_dashboard_atlet(request):
-    cursor = connection.cursor()
-    cursor.execute("""
-        SELECT
-            M.Nama AS "Nama lengkap",
-            A.Negara_Asal AS "Negara",
-            M.Email AS "Email",
-            A.Tgl_Lahir AS "Tanggal lahir",
-            CASE
-                WHEN A.Play_Right THEN 'Right Hand'
-                ELSE 'Left Hand'
-            END AS "Play",
-            A.Height AS "Tinggi badan",
-            CASE
-                WHEN A.Jenis_Kelamin THEN 'Laki-laki'
-                ELSE 'Perempuan'
-            END AS "Jenis Kelamin",
-            COALESCE(P.Memiliki_Pelatih, '-') AS "Pelatih",
-            CASE
-                WHEN AK.ID_Atlet IS NOT NULL THEN 'Qualified'
-                ELSE 'Not Qualified'
-            END AS "Status",
-            COALESCE(CAST(A.World_Rank AS VARCHAR), '-') AS "World rank",
-            COALESCE(PH.Total_Point, 0) AS "Total poin"
-        FROM
-            ATLET A
-            INNER JOIN MEMBER M ON A.ID = M.ID
-            LEFT JOIN ATLET_KUALIFIKASI AK ON A.ID = AK.ID_Atlet
-            LEFT JOIN (
-                SELECT ID_Pelatih, 'Yes' AS Memiliki_Pelatih
-                FROM ATLET_PELATIH
-            ) P ON A.ID = P.ID_Pelatih
-            LEFT JOIN POINT_HISTORY PH ON A.ID = PH.ID_Atlet
-        ORDER BY
-            CASE
-                WHEN A.World_Rank IS NULL THEN 1
-                ELSE 0
-            END,
-            A.World_Rank,
-            M.Nama;
+    data = execute(f"""
+    SELECT
+  M.Nama AS Nama_Lengkap,
+  A.Negara_Asal AS Negara,
+  M.Email,
+  A.Tgl_Lahir AS Tanggal_Lahir,
+  CASE WHEN A.Play_Right THEN 'Right Hand' ELSE 'Left Hand' END AS Play,
+  A.Height AS Tinggi_Badan,
+  CASE WHEN A.Jenis_Kelamin THEN 'Male' ELSE 'Female' END AS Jenis_Kelamin,
+  COALESCE(MC.Nama, '-') AS Pelatih,
+  CASE
+    WHEN AK.ID_Atlet IS NULL THEN 'Not Qualified'
+    ELSE COALESCE(CAST(A.World_Rank AS VARCHAR), '-')
+  END AS Status,
+  COALESCE(CAST(A.World_Rank AS VARCHAR), '-') AS World_Rank,
+  SUM(COALESCE(PH.Total_Point, 0)) AS Total_Poin
+FROM
+  ATLET A
+  INNER JOIN MEMBER M ON A.ID = M.ID
+  LEFT JOIN ATLET_PELATIH AP ON A.ID = AP.ID_Atlet
+  LEFT JOIN MEMBER MC ON AP.ID_Pelatih = MC.ID
+  LEFT JOIN ATLET_KUALIFIKASI AK ON A.ID = AK.ID_Atlet
+  LEFT JOIN POINT_HISTORY PH ON A.ID = PH.ID_Atlet
+WHERE
+  M.ID = '1509b2a8-422a-4cde-bf1b-f289c267d494'
+GROUP BY
+  M.Nama,
+  A.Negara_Asal,
+  M.Email,
+  A.Tgl_Lahir,
+  A.Play_Right,
+  A.Height,
+  A.Jenis_Kelamin,
+  A.World_Rank,
+  MC.Nama,
+  AK.ID_Atlet;
+
     """)
-    data = cursor.fetchall()
-    columns = [column[0] for column in cursor.description]
+    print(data)
     context = {
-        'columns': columns,
-        'data': data
+        'data': data,
     }
-    return render(request, 'dashboard_atlet.html', context)
+    return render(request, "dashboard_atlet.html", context)
+
+
+def pilih_partai(request, pk):
+    event = execute(f"""
+        SELECT Nama_Event, Total_Hadiah, Tgl_Mulai, Tgl_Selesai, Kategori_superseries, Nama_Stadium, event.Negara, s.kapasitas
+        FROM EVENT
+        JOIN stadium s ON s.nama = event.nama_stadium
+        WHERE Nama_Event = '{pk}';
+    """)
+    context = {
+        'event': event,
+    }
+    return render(request,"pilih_partai.html", context)
