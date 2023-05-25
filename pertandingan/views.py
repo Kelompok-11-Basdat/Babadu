0215a328435c53cf85ad701a0c4d7b8421f874f3
daffarestupratama <daffa.ilham@ui.ac.id>
2023-05-25 23:43:13 +0700

    Add query

--- pertandingan/views.py
+++ pertandingan/views.py
@@ -2,45 +2,83 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from django.shortcuts import render, redirect
-from django.db import connection, InternalError
+# from django.db import connection, InternalError
 
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib import messages
 
 from util.query import *
-from atlet.query import *
-from Babadu.helper.function import *
+# from atlet.query import *
+# from Babadu.helper.function import *
 
-# Create your views here.
+@csrf_exempt
 def pertandingan_perempat(request):
 
-    
+    if request.method == 'POST':
+        nama_event = request.POST.get("nama_event")
+        tahun_event = request.POST.get("tahun_event")
 
-    context = {
-        'role' : 'Umpire',
-    }
-    return render(request, 'pertandingan_perempat.html', context)
+        # nama_event = "Net Nuisance"
+        # tahun_event = 2020
 
+        query = """
+            -- Atlet Ganda
+            (SELECT M.Nama
+            FROM ATLET_GANDA AS AG, MEMBER AS M
+            WHERE (AG.ID_Atlet_Kualifikasi = M.ID OR AG.ID_Atlet_Kualifikasi_2 = M.ID)
+            AND AG.ID_Atlet_Ganda IN
+                (SELECT PK.ID_Atlet_Ganda
+                FROM PESERTA_KOMPETISI AS PK
+                WHERE PK.ID_Atlet_Ganda IS NOT NULL
+                AND Nomor_Peserta IN
+                    (SELECT PPK.Nomor_Peserta
+                    FROM PARTAI_PESERTA_KOMPETISI AS PPK
+                    WHERE PPK.Nama_Event = '{nama_event}'
+                    AND PPK.Tahun_Event = {tahun_event})))
+            UNION
+            -- Atlet tunggal
+            (SELECT M.Nama
+            FROM MEMBER AS M
+            WHERE 
+            AND M.ID IN
+                (SELECT PK.ID_Atlet_Kualifikasi
+                FROM PESERTA_KOMPETISI AS PK
+                WHERE PK.ID_Atlet_Ganda IS NULL
+                AND Nomor_Peserta IN
+                    (SELECT PPK.Nomor_Peserta
+                    FROM PARTAI_PESERTA_KOMPETISI AS PPK
+                    WHERE PPK.Nama_Event = '{nama_event}'
+                    AND PPK.Tahun_Event = {tahun_event})))
+        """
 
+        data = execute(query=query)
+        # print(data)
+
+        context = {
+            data : data,
+        }
+        return render(request, 'pertandingan_perempat.html', context)
+
+@csrf_exempt
 def pertandingan_semifinal(request):
+    if request.method == 'POST':
 
-    context = {
-        'role' : 'Umpire',
-    }
-    return render(request, 'pertandingan_semifinal.html', context)
+        context = {
 
+        }
+        return render(request, 'pertandingan_semifinal.html', context)
 
+@csrf_exempt
 def pertandingan_juara_tiga(request):
+    if request.method == 'POST':
+        context = {
 
-    context = {
-        'role' : 'Umpire',
-    }
-    return render(request, 'pertandingan_juara_tiga.html', context)
+        }
+        return render(request, 'pertandingan_juara_tiga.html', context)
 
-
+@csrf_exempt
 def pertandingan_final(request):
+    if request.method == 'POST':
+        context = {
 
-    context = {
-        'role' : 'Umpire',
-    }
-    return render(request, 'pertandingan_final.html', context)
+        }
+        return render(request, 'pertandingan_final.html', context)
